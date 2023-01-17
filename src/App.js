ce2cb62966faac0c3658e4133ecec83c9b8cf73f
Rakooa37 <118366375+Rakooa37@users.noreply.github.com>
2023-01-17 20:08:00 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,15 +9,12 @@ import PersonalExperienceForm from './Components/PersonalExperienceForm';
 import Button from './Components/Button';
 import PersonalExperienceDisplay from './Components/PersonalExperienceDisplay';
 import { exportComponentAsPDF } from 'react-component-export-image'
-import cvDisplay from './Components/CvDisplay';
 import EducationForm from './Components/EducationForm';
 
 
 
 export default function App() {
-  
 
-  //add experience and personal information section
   const ref = useRef(0);
   const pdf = useRef()
   
@@ -28,11 +25,11 @@ export default function App() {
     
   }
   const [image, setImage] = useState("avatar.jpg")
-  const [personalExperience, setPersonalExperience] = useState({'position': "", 'company': "", 'city':"", 'from':"", 'to':"", 'descripton': ''});
-  const [personalExperiences, setPersonalExperiences] = useState([{'position': "", 'company': "", 'city':"", 'from':"", 'to':"", 'descripton': ''}]);
+  const [personalExperience, setPersonalExperience] = useState({'position': "", 'company': "", 'city':"", 'from':"", 'to':"", 'description': ''});
+  const [personalExperiences, setPersonalExperiences] = useState([{'position': "", 'company': "", 'city':"", 'from':"", 'to':"", 'description': ''}]);
   const [personalExperienceForms, setpersonalExperienceForms] = useState([<PersonalExperienceForm key = {0} id = {0} onChange={handleChangePersonalExperience}/>]);
   const [personalInfo, setPersonalInfo] = useState({firstName: '', lastName: '', title: '', address: '', phoneNumber: '', email: '', description: ''}); 
-  const [personalExperienceDisplays, setPersonalExperienceDisplays] = useState([<PersonalExperienceDisplay key={0} id = {0} data={{'position': "", 'company': "", 'city':"", 'from':"", 'to':"", 'descripton': ''}}/>]);
+  const [personalExperienceDisplays, setPersonalExperienceDisplays] = useState([<PersonalExperienceDisplay key={0} id = {0} data={{'position': "", 'company': "", 'city':"", 'from':"", 'to':"", 'description': ''}}/>]);
   const [education, setEducation] = useState({'university': '', "subject": '', 'city':'', 'from':'', 'to':''})
   
   const handleChangePersonalInfo = (e)=>{
@@ -105,7 +102,7 @@ export default function App() {
 
   const handleAddClick = ()=>{
     const newPersonalExperienceForm = <PersonalExperienceForm key={personalExperienceForms.length} id = {personalExperienceForms.length} onChange={handleChangePersonalExperience}/>;
-    const newPersonalExperienceDisplay = <PersonalExperienceDisplay key={personalExperienceDisplays.length} id = {personalExperienceDisplays.length} data = {{'position': "", 'company': "", 'city':"", 'from':"", 'to':"", 'descripton': ''}}/>;
+    const newPersonalExperienceDisplay = <PersonalExperienceDisplay key={personalExperienceDisplays.length} id = {personalExperienceDisplays.length} data = {{'position': "", 'company': "", 'city':"", 'from':"", 'to':"", 'description': ''}}/>;
     setpersonalExperienceForms([...personalExperienceForms, newPersonalExperienceForm])
 
     setPersonalExperienceDisplays((prev)=>{
@@ -116,7 +113,7 @@ export default function App() {
     
     setPersonalExperiences((prev)=>{
       let newArray = [...prev];
-      newArray[personalExperienceForms.length] = {'position': "", 'company': "", 'city':"", 'from':"", 'to':"", 'descripton': ''};
+      newArray[personalExperienceForms.length] = {'position': "", 'company': "", 'city':"", 'from':"", 'to':"", 'description': ''};
       return newArray
     })
   }
@@ -136,7 +133,26 @@ export default function App() {
     }
   }
 
-  //----------------------------------------------------------------------------------------------------------------------------------------------------------------
+  const handleLoadExampleClick = (event)=>{
+    setPersonalInfo({'firstName': 'Ramira', 'lastName': 'Jhones', 'title': 'Rpa Developer', 'address': 'Rio Cauto, Cuba', 'phoneNumber': '+4074349027', 'email': 'ramirajhones@hotmail.com', 'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur'})
+    setPersonalExperiences([{'position': "Rpa developer", 'company': "Deloite", 'city':"Bucharest", 'from':"June 2020", 'to':"May 2021", 'description': `- Reduced manual effort by developing an automation solution for checking scanned documents using OCR and sending results via email Developed web
+    - Developed web data scraping and Excel automations to remove manual labor
+    -Developed pdf automations and data validation using RegEx
+    - Developed Excel and Google Sheets macros
+    -Data analytics (QlikView) - took part in real business projects for
+    clients like a stock management dashboard for a car distribution
+    company`}
+    ])
+    setImage("download.jpg")
+    setEducation({'university': 'Cambridge', "subject": 'Computer Science', 'city':'London', 'from':'June 2015', 'to':'September 2019'})
+  }
+
+  const handleResetClick = (event)=>{
+    setPersonalInfo({firstName: '', lastName: '', title: '', address: '', phoneNumber: '', email: '', description: ''})
+    setPersonalExperiences([{'position': "", 'company': "", 'city':"", 'from':"", 'to':"", 'description': ''}])
+    setEducation({'university': '', "subject": '', 'city':'', 'from':'', 'to':''})
+    setImage("avatar.jpg")
+  }
   
 
   
@@ -152,7 +168,8 @@ export default function App() {
           <Button color = "#4a9e47" text = "Add experience" onClick = {handleAddClick}></Button>
           <Button color = "#c74655" text = "Delete" onClick={handleDeleteClick}></Button>
           <Button color = "#7d94b5" text = "Generate PDF" onClick={()=>exportComponentAsPDF(pdf, {fileName:'Curriculum Vitae', pdfOptions: { w: 210, h: 297 }})}></Button>
-
+          <Button color = "#2f47bd" text = "Load Example" onClick={handleLoadExampleClick}/>
+          <Button color = "#1e8e94" text = "Reset CV" onClick={handleResetClick}/>
         </div>
 
         <CvDisplay innerRef = {pdf} personalData = {personalInfo} personalExperienceDisplays = {personalExperienceDisplays} handleImageUpload = {handleImageUpload} image = {image} education = {education}/>
